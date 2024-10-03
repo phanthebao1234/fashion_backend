7b6ffa3847f29a2525594867a45cbbff258058bd
phanthebao1234 <phanthebao888@gmail.com>
2024-10-03 22:46:17 +0700

    add notification serializers

--- notification/serializers.py
+++ notification/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from . import models
 
-class NotificationSerializer(serializers.Serializer):
-    model = models.Notification
-    fields = '__all__'
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Notification
+        fields = '__all__'
     
